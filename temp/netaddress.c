2e8561bbce60ebd60ddf097742e41a7da327df53
ppna <3137908255@qq.com>
2019-03-31 11:56:03 +0800

    edit net name

--- temp/netaddress.c
+++ temp/netaddress.c
@@ -1,31 +1,6 @@
-funcNet1()
-{
-	Name=xiaomi小米
-	Address=http://www.xiaomi.com	
-}
-funcNet2()
-{
-	Name=baidu百度
-	Address=http://www.baidu.com
-}
-funcNet3()
-{
-	Name=github
-	Address=http://www.github.com	
-}
-funcNet4()
-{
-	Name=xiaomi小米
-	Address=http://www.xiaomi.com	
-}
-funcNet5()
-{
-	Name=baidu百度
-	Address=http://www.baidu.com
-}
-funcNet6()
-{
-	Name=github
-	Address=http://www.github.com	
-}
-
+Name=xiaomi小米=Address=http://www.xiaomi.com	
+Name=baidu百度=Address=http://www.baidu.com
+Name=github=Address=http://www.github.com
+Name=xiaomi小米=Address=http://www.xiaomi.com
+Name=baidu百度=Address=http://www.baidu.com
+Name=github=Address=http://www.github.com
