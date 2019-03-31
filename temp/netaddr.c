92807b4da6c382e6364cfbb2a3d781d855db895b
ppna <3137908255@qq.com>
2019-03-31 11:21:54 +0800

    edit net name

--- temp/netaddr.c
+++ temp/netaddr.c
@@ -1,6 +1,6 @@
-NetName1=xiaomi
+NetName1=xiaomi小米
 NetAddress1=http://www.xiaomi.com
-NetName2=baidu
+NetName2=baidu百度
 NetAddress2=http://www.baidu.com
 NetName3=github
 NetAddress3=http://www.github.com
